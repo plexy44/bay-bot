79a995664cb3150cb3f0fcc040ca68901b847598
Salman Khan <khansolo23@gmail.com>
2025-06-13 03:51:56 +0000

    - light and dark modes toggle is not working - ensure testing and bugs i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>{/* suppressHydrationWarning for theme changes */}
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
